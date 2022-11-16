f896b69b9d0df508b5603cc446615a0cb895a42c
victor-vieira21 <vitoraugusto_2006@hotmail.com>
2022-11-16 20:02:58 -0300

    first commit

---
+++ app/Controllers/Http/PostsController.ts
@@ -0,0 +1,40 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
+
+export default class PostsController {
+  public async index({}: HttpContextContract) {
+    const posts = await Post.query().orderBy('id')
+
+    return posts
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'content'])
+    const post = await Post.create(data)
+
+    return post
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+
+    return post
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    const data = request.only(['title', 'content'])
+
+    post.merge(data)
+
+    await post.save()
+
+    return post
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+
+    await post.delete()
+  }
+}
---
+++ app/Models/Post.ts
@@ -0,0 +1,23 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  // @column({ columnName: 'content' })
+  // public title: string
+  //caso queira usar outro nome para ligar a coluna
+
+  @column()
+  public content: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
